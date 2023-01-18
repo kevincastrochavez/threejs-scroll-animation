be430f7858f40e79815344b2d26a050b7b9d06ce
Kevin <kevin.castro.chavez0@gmail.com>
2023-01-18 16:12:58 -0700

    created mesh objects and material, and added directional light

--- src/script.js
+++ src/script.js
@@ -22,14 +22,26 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// Objects
+const material = new THREE.MeshToonMaterial({
+  color: parameters.materialColor,
+});
+
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
+
+const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
+
+const mesh3 = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+  material
 );
-scene.add(cube);
+
+scene.add(mesh1, mesh2, mesh3);
+
+// Lights
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
 
 /**
  * Sizes
